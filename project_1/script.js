1050dcc2eed3f649d3d89eff4a4ec28963b25f94
anchalDev159357 <singhanchal159357@gmail.com>
2023-08-15 08:06:51 +0530

    added changes

---
+++ project_1/script.js
@@ -0,0 +1,3 @@
+Newqoutes = document.onclick.querySelector('.new-quote').textContent  = ("Don't wait a perfect moment just take moment and make it perfect.");;
+
+document.querySelector('#qoute').onclick(Newqoutes)
